8c1e21258654f6b51681cedd06b0e63b8d27ac00
Kensuke Shibata <kensukeshibata@gmail.com>
2019-08-05 15:43:44 +1000

    fetching youtube API

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,18 @@
+import axios from 'axios'
+
+
 const KEY = "AIzaSyCHn_g6G1_esoELwnxNrGb1lTF0Za-yQrc"
+
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    maxResults: 5,
+    key: KEY,
+  }
+})
+
+
+
+
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -11,6 +11,8 @@ class SearchBar extends React.Component {
   onFormSubmit = (event) => {
     event.preventDefault();
 
+    this.props.onFormSubmit(this.state.term);
+
     //Make sure we call
     //callback from parent component
 
